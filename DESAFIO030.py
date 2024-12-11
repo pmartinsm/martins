70af5d5a6ab4f1ad2fa31a126b0efd30d3d6235e
Pedro Lucas Martins Pereira <fadbaacad@gmail.com>
2024-12-11 09:04:02 -0300

    Update DESAFIO030.py

--- DESAFIO030.py
+++ DESAFIO030.py
@@ -1,5 +1,7 @@
-num = int(input('Me diga um numero qualquer: '))
+from colorama import Fore, Style
+
+num = int(input(f'{Fore.LIGHTMAGENTA_EX}Me diga um numero qualquer: {Style.RESET_ALL}'))
 if num % 2 == 0:
-    print(f'O numero {num} é PAR')
+    print(f'O numero {num} é {Fore.BLUE}PAR{Style.RESET_ALL}')
 else:
-    print(f'O numero {num} é IMPAR')
+    print(f'O numero {num} é {Fore.BLUE}IMPAR{Style.RESET_ALL}')
